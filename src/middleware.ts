ddd457261b92138bb17da57e9d90ef3cc6a0bfe4
karimscoding <rejaulkarim66666@gmail.com>
2024-04-28 15:24:04 +0800

    fix: fix auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,22 +2,7 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: [
-    '/',
-    '/blog',
-    '/blog/:id',
-    '/terms',
-    '/privacy',
-    '/cookies',
-    '/membership',
-    '/all-nextjs-boilerplate-and-starterkit',
-    '/all-nextjs-boilerplate-and-starterkit/:id',
-    '/premium-nextjs-boilerplate-and-starterkit',
-    '/premium-nextjs-boilerplate-and-starterkit/:id',
-    '/free-nextjs-boilerplate-and-starterkit',
-    '/free-nextjs-boilerplate-and-starterkit/:id',
-    '/contact',
-  ],
+  publicRoutes: ['/api/wehook/clerk'],
 
   // Routes that can always be accessed, and have
   // no authentication information
