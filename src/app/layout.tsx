1b0c651d8820479cc54083ee1cc34fa31d7543a6
karimscoding <rejaulkarim66666@gmail.com>
2024-04-29 01:16:57 +0800

    feat: clerk updated to latest version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { siteConfig } from '@/config/site';
 import '@/styles/globals.css';
 import { cn } from '@/utils/utils';
 import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'sonner';
@@ -63,11 +62,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
-    >
+    <ClerkProvider>
       <html lang="en">
         <body className={cn('antialiased', font.className)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,7 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/api/webhook/clerk'],
-
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/api/og', '/api/webhook/clerk'],
-});
+export default clerkMiddleware();
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
