844b0dbcd178fbb1115efc148bdbf312a803568a
karimscoding <rejaulkarim66666@gmail.com>
2024-04-28 15:59:09 +0800

    fix: type error

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/api/wehook/clerk'],
+  publicRoutes: ['/', '/api/webhook/clerk'],
 
   // Routes that can always be accessed, and have
   // no authentication information
