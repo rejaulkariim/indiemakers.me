1c14645e89fe74b9bfd3cd7d65cdabc2b8c80ec8
karimscoding <rejaulkarim66666@gmail.com>
2024-05-06 15:45:46 +0800

    feat: enable error page

---
+++ src/app/error.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <Button variant="destructive" onClick={() => reset()}>
+        Try again
+      </Button>
+    </div>
+  );
+}
