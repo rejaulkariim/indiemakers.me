35dfadef499492290950eb00dd96b076f422e9f9
karimscoding <rejaulkarim66666@gmail.com>
2024-06-30 21:09:47 +0800

    fix: fix type error from admin dashboard page

--- src/app/(dashboard)/admin/dashboard/page.tsx
+++ src/app/(dashboard)/admin/dashboard/page.tsx
@@ -1,5 +1,5 @@
+import { getUserById } from '@/server/modules/user/user.actions'
 import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
 import { redirect } from 'next/navigation'
 
 const AdminDashboardPage = async () => {
