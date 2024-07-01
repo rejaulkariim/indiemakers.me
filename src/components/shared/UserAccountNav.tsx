3887e58ce5817da86731636fcfa93c3679213c64
karimscoding <rejaulkarim66666@gmail.com>
2024-07-01 23:48:12 +0800

    fix: fix type error from userAcountNav

--- src/components/shared/UserAccountNav.tsx
+++ src/components/shared/UserAccountNav.tsx
@@ -3,20 +3,20 @@ import {
   DropdownMenuContent,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { getUserById } from '@/lib/actions/user.actions';
-import { cn } from '@/utils/utils';
-import { SignOutButton } from '@clerk/nextjs';
-import { auth } from '@clerk/nextjs/server';
-import { LayoutGrid, LogOut } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { getUserById } from '@/server/modules/user/user.actions'
+import { cn } from '@/utils/utils'
+import { SignOutButton } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
+import { LayoutGrid, LogOut } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const UserAccountNav = async () => {
-  const { userId: clerkId } = auth();
+  const { userId: clerkId } = auth()
 
-  const mongoUser = await getUserById({ userId: clerkId });
+  const mongoUser = await getUserById({ userId: clerkId })
   //   console.log(mongoUser.role !== 'admin', 'mongouser from admin dashboard');
 
   return (
@@ -27,40 +27,40 @@ const UserAccountNav = async () => {
             src={mongoUser?.photo}
             height={100}
             width={100}
-            alt="user"
-            className="h-8 w-8 object-contain rounded-full cursor-pointer focus:ring"
+            alt='user'
+            className='h-8 w-8 cursor-pointer rounded-full object-contain focus:ring'
           />
         </DropdownMenuTrigger>
         <DropdownMenuContent
-          className="w-52 bg-card rounded-lg mt-4"
-          side="bottom"
+          className='mt-4 w-52 rounded-lg bg-card'
+          side='bottom'
         >
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
 
           <div>
             {mongoUser?.role === 'admin' ? (
-              <Link href="/admin/dashboard/">
+              <Link href='/admin/dashboard/'>
                 <span
                   className={cn(
                     'group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground'
                   )}
                 >
-                  <LayoutGrid className="mr-2 h-4 w-4 text-muted-foreground" />
-                  <span className="text-sm text-muted-foreground">
+                  <LayoutGrid className='mr-2 h-4 w-4 text-muted-foreground' />
+                  <span className='text-sm text-muted-foreground'>
                     Dashboard
                   </span>
                 </span>
               </Link>
             ) : (
-              <Link href="/user/dashboard/profile">
+              <Link href='/user/dashboard/profile'>
                 <span
                   className={cn(
                     'group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground'
                   )}
                 >
-                  <LayoutGrid className="mr-2 h-4 w-4 text-muted-foreground" />
-                  <span className="text-sm text-muted-foreground">
+                  <LayoutGrid className='mr-2 h-4 w-4 text-muted-foreground' />
+                  <span className='text-sm text-muted-foreground'>
                     Dashboard
                   </span>
                 </span>
@@ -88,9 +88,9 @@ const UserAccountNav = async () => {
                 'group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground'
               )}
             >
-              <LogOut className="mr-2 h-4 w-4 text-muted-foreground" />
+              <LogOut className='mr-2 h-4 w-4 text-muted-foreground' />
               <SignOutButton>
-                <button className="text-sm text-muted-foreground">
+                <button className='text-sm text-muted-foreground'>
                   Logout
                 </button>
               </SignOutButton>
@@ -99,7 +99,7 @@ const UserAccountNav = async () => {
         </DropdownMenuContent>
       </DropdownMenu>
     </>
-  );
-};
+  )
+}
 
-export default UserAccountNav;
+export default UserAccountNav
