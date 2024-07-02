0ecbd03ea0b19659f46d83a995c00f77c9ff4442
karimscoding <rejaulkarim66666@gmail.com>
2024-07-03 02:04:54 +0800

    update: updated blog

--- src/app/(root)/blog/page.tsx
+++ src/app/(root)/blog/page.tsx
@@ -1,41 +1,39 @@
-import { allPosts } from 'contentlayer/generated';
-import { compareDesc } from 'date-fns';
-import Image from 'next/image';
-import Link from 'next/link';
+import { allPosts } from 'contentlayer/generated'
+import { compareDesc } from 'date-fns'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import MaxWidthWrapper from '@/components/shared/MaxWidthWrapper';
-import SectionHeader from '@/components/shared/SectionHeader';
-import { formatDate } from '@/utils/utils';
+import MaxWidthWrapper from '@/components/shared/MaxWidthWrapper'
+import { formatDate } from '@/utils/utils'
 
 export const metadata = {
-  title: 'Blog',
-};
+  title: 'Blog'
+}
 
 export default async function BlogPage() {
   const posts = allPosts
-    .filter((post) => post.published)
+    .filter(post => post.published)
     .sort((a, b) => {
-      return compareDesc(new Date(a.date), new Date(b.date));
-    });
+      return compareDesc(new Date(a.date), new Date(b.date))
+    })
 
   return (
-    <section className="section-padding">
+    <section className='section-padding'>
       <MaxWidthWrapper>
-        <SectionHeader
-          title={
-            <span>
-              Newest <span className="font-bold">Technology</span> Trends
-            </span>
-          }
-          subtitle="Essential Tips and Tricks for Indie Makers' Latest Releases"
-        />
-
+        <div className='space-y-2'>
+          <h1 className='text-3xl font-bold md:text-4xl'>Blogs</h1>
+          <p className='max-w-prose text-lg text-muted-foreground'>
+            We share in-depth reviews and essential information about every
+            product listed on our platform, ensuring you have all the details
+            you need to make informed decisions.
+          </p>
+        </div>
         {posts?.length ? (
-          <div className="grid gap-10 sm:grid-cols-3 my-10">
+          <div className='my-10 grid grid-cols-1 gap-10 md:grid-cols-3'>
             {posts.map((post, index) => (
               <article
                 key={post._id}
-                className="group relative flex flex-col space-y-2"
+                className='group relative flex flex-col space-y-2'
               >
                 {post.image && (
                   <Image
@@ -43,25 +41,23 @@ export default async function BlogPage() {
                     alt={post.title}
                     width={804}
                     height={452}
-                    className="h-56 rounded-xl border bg-muted transition-colors"
+                    className='h-56 rounded-xl border bg-muted transition-colors'
                     priority={index <= 1}
                   />
                 )}
-                <h2 className="text-lg font-bold">
-                  {post.title.slice(0, 40)}...
-                </h2>
+                <h3 className='text-xl font-bold'>{post.title}</h3>
                 {post.description && (
-                  <p className="paragraph">
+                  <p className='text-muted-foreground'>
                     {post.description.slice(0, 100)}...
                   </p>
                 )}
                 {post.date && (
-                  <p className="paragraph text-xs font-bold">
+                  <p className='paragraph text-xs font-bold'>
                     {formatDate(post.date)}
                   </p>
                 )}
-                <Link href={post.slug} className="absolute inset-0">
-                  <span className="sr-only">View Article</span>
+                <Link href={post.slug} className='absolute inset-0'>
+                  <span className='sr-only'>View Article</span>
                 </Link>
               </article>
             ))}
@@ -71,5 +67,5 @@ export default async function BlogPage() {
         )}
       </MaxWidthWrapper>
     </section>
-  );
+  )
 }
--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ import { SiteConfig } from '@/types'
 
 export const siteConfig: SiteConfig = {
   name: 'Indie Makers',
-  title: 'Find The Best New Products in Tech.',
+  title: 'Find The Best AI Tools & New Products in Tech.',
   description:
     'Explore a vast collection of AI Tools and Tech Products to enhance your workflow and unlock new possibilities.',
   url: 'https://indiemakers.me',
