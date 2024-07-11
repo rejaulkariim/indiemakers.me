cf90ab624f7985daef2f0fc4c70d70bf8fe39249
karimscoding <rejaulkarim66666@gmail.com>
2024-07-11 18:57:08 +0800

    chore: updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isProtectedRoute = createRouteMatcher([
+  '/product(.*)',
   '/submit',
   '/user/dashboard(.*)',
-  '/admin/dashboard(.*)',
-]);
+  '/admin/dashboard(.*)'
+])
 
 export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) auth().protect();
-});
+  if (isProtectedRoute(request)) auth().protect()
+})
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
+}
