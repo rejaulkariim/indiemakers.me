63ac932679608a2ad3ab572744ffc570e3c837fb
karimscoding <rejaulkarim66666@gmail.com>
2024-10-07 20:48:03 +0600

    update: update

--- src/components/design/products/ProductReels.tsx
+++ src/components/design/products/ProductReels.tsx
@@ -5,31 +5,27 @@ import NoResult from '@/components/shared/NoResult'
 const ProductReels = async ({ result }: any) => {
   return (
     <section className='section-padding'>
-      <MaxWidthWrapper className='max-w-screen-xl'>
-        <h3 className='text-2xl font-bold md:text-3xl'>Explore</h3>
+      <MaxWidthWrapper>
         {result.length > 0 ? (
-          <div className='mt-4 flex flex-col items-center gap-10 md:mt-10'>
-            <ul className='grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3'>
-              {result.map((product: any) => {
-                return (
-                  <li key={product._id} className='flex justify-center'>
-                    <ProductCard
-                      key={product._id}
-                      name={product.name}
-                      slug={product.slug}
-                      title={product.title}
-                      logo={product.logo}
-                      tags={product.tags}
-                      status={product.status}
-                      upvotes={product.upvotes}
-                      views={product.views}
-                      comments={product.comments}
-                    />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+          <ul className='mt-4 flex flex-col items-center gap-10 md:mt-10'>
+            {result.map((product: any) => {
+              return (
+                <li key={product._id} className='flex justify-center'>
+                  <ProductCard
+                    name={product.name}
+                    slug={product.slug}
+                    title={product.title}
+                    logo={product.logo}
+                    tags={product.tags}
+                    status={product.status}
+                    upvotes={product.upvotes}
+                    views={product.views}
+                    comments={product.comments}
+                  />
+                </li>
+              )
+            })}
+          </ul>
         ) : (
           <NoResult
             emptyTitle='No result found'
